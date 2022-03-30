3c90d4153d85434328301e2fa61343c7836da458
"Nick Fabrizio" <"nicholas.fabrizio@gmail.com">
2022-03-30 14:58:49 -0600

    Updates AuthzKey contract to add auth key as token URI

--- contracts/AuthzKey.sol
+++ contracts/AuthzKey.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+// import "./Base64.sol";
 
 contract AuthzKey is ERC721URIStorage {
   using Counters for Counters.Counter;
@@ -13,6 +14,20 @@ contract AuthzKey is ERC721URIStorage {
 
   constructor() ERC721("AuthzKey", "AUTHZ_KEY") {
   }
+
+  // function getTokenURI(string memory data) public returns (string memory) {
+  //   return
+  //       string(
+  //         abi.encodePacked(
+  //           "data:application/json;base64,",
+  //           Base64.encode(
+  //             bytes(
+  //               abi.encodePacked('{"key":"', data, '"}')
+  //             )
+  //           )
+  //         )
+  //       );
+  // }
 
   function transferAuthzKey(address _newOwner, string memory _authZKey) public returns (uint256) {
     return this.mint(_newOwner, _authZKey);
@@ -24,15 +39,19 @@ contract AuthzKey is ERC721URIStorage {
     uint256 newItemId = _tokenIds.current();
     authZKeys.push(authZKey);
 
-    string memory tokenURI = string(abi.encodePacked(_baseURL, newItemId, ".json")); 
+    // string memory tokenURI = string(abi.encodePacked(_baseURL, newItemId, ".json")); 
 
     // Create json file with content as authzKey with proper name and put it at _baseURL
+    //   - Start with plain text, possibly use IPFS
+    //   - Store encrypted token later
+    // OR base64 encode the access key and add the encoded value as the tokenURI
     // When token URI is accessed, check token owner before returning content
     // Burn token after it is accessed
 
     //Call the mint function
     _mint(newOwner, newItemId);
-    _setTokenURI(newItemId, tokenURI);
+    // _setTokenURI(newItemId, tokenURI);
+    _setTokenURI(newItemId, authZKey);
 
     return newItemId;
   }
